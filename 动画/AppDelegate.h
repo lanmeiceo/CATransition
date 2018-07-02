26f06dccc5ac13716ef91555d46af7f66d09930b
caowei <751484591@qq.com>
2018-07-02 15:13:47 +0800

    Initial Commit

---
+++ 动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  动画
+//
+//  Created by BlueBerry on 2018/7/2.
+//  Copyright © 2018年 蓝莓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 动画/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  动画
+//
+//  Created by BlueBerry on 2018/7/2.
+//  Copyright © 2018年 蓝莓. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
